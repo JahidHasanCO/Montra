cd5bd8579d3f744fa610fd51a091176be6387535
Md. Zahidul Islam <vdjsovaj@gmail.com>
2021-11-05 14:44:31 +0600

    Create Project, Create Splash Screen Activity. Update All Colors , update theme.

---
+++ app/src/main/java/dev/jahidhasanco/montra/presentation/activity/GetStartedActivity.kt
@@ -0,0 +1,12 @@
+package dev.jahidhasanco.montra.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.jahidhasanco.montra.R
+
+class GetStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+    }
+}
---
+++ app/src/main/java/dev/jahidhasanco/montra/presentation/activity/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package dev.jahidhasanco.montra.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.jahidhasanco.montra.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
