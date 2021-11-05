1b40190049ea47600fa478a9b9e17f21639136f5
Md. Zahidul Islam <vdjsovaj@gmail.com>
2021-11-05 15:37:49 +0600

    Create Navigation Host And GetStarted Nav Graph

--- app/src/main/java/dev/jahidhasanco/montra/presentation/activity/SplashScreenActivity.kt
+++ app/src/main/java/dev/jahidhasanco/montra/presentation/activity/SplashScreenActivity.kt
@@ -2,11 +2,15 @@ package dev.jahidhasanco.montra.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import dev.jahidhasanco.montra.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
     }
 }
